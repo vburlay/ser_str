4cc1936cafd38c30e18f5e89571a017e81b300f2
gordon_soic@mail.ru <gordon_soic@mail.ru>
2023-02-13 17:58:12 +0100

    Initial commit

---
+++ code/skript_audio_class.py
@@ -0,0 +1,84 @@
+"""
+Created on Sat Feb 03 21:41:46 2023
+
+@author: Vladimir
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+import torch
+import json
+from torch import nn
+from torch import optim
+import numpy as np
+from PIL import Image
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+from os import walk
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+import shutil
+
+
+def check_wav(path):
+    PATH_TO_FILE = Path.cwd() / path
+    files = os.listdir(PATH_TO_FILE)
+    newpath = PATH_TO_FILE / 'temp'
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+    for file_name in files:
+        full_file_name = os.path.join(PATH_TO_FILE, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, newpath)
+
+    class_names = ['neutral', 'happy', 'sad', 'angry', 'fear', 'surprise']
+    classification = []
+    percentages_0 = []
+    percentages_1 = []
+    percentages_2 = []
+    percentages_3 = []
+    percentages_4 = []
+    percentages_5 = []
+
+    # load model
+    model = torch.load('models/audio_model.pth')
+    device = "cpu"
+
+    image_transformed = transforms.Compose([transforms.Resize(255),
+                                            transforms.CenterCrop(224),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize([0.485, 0.456, 0.406],
+                                                                 [0.229, 0.224, 0.225])])
+
+    images = datasets.ImageFolder(PATH_TO_FILE, transform=image_transformed)
+    image_loader = torch.utils.data.DataLoader(images, batch_size=1000)
+    model.eval()
+    images, labels = next(iter(image_loader))
+    images, labels, model = images.to(device), labels.to(device), model.to(device)
+
+    ps = torch.exp(model(images))
+    top_p, top_class = ps.topk(1, dim=1)
+    equals = top_class == labels.view(*top_class.shape)
+    for i in range(0, len(top_class.tolist())):
+        # classification.extend([str(top_class.tolist()[i][0])])
+
+        score = tf.nn.softmax(ps.tolist()[i])
+        classification.extend([class_names[np.argmax(score.numpy())]])
+        percentages_0.extend([str(round(score.numpy()[0], 3))])
+        percentages_1.extend([str(round(score.numpy()[1], 3))])
+        percentages_2.extend([str(round(score.numpy()[2], 3))])
+        percentages_3.extend([str(round(score.numpy()[3], 3))])
+        percentages_4.extend([str(round(score.numpy()[4], 3))])
+        percentages_5.extend([str(round(score.numpy()[5], 3))])
+
+    for f in os.listdir(newpath):
+        os.remove(os.path.join(newpath, f))
+    os.rmdir(newpath)
+    data = {'file': files, 'classification': classification, 'neutral': percentages_0,
+            'happy': percentages_1, 'sad': percentages_2, 'angry': percentages_3,
+            'fear': percentages_4, 'surprise': percentages_5}
+    df_results = pd.DataFrame(data=data)
+    return df_results
---
+++ sample/skript_wav_png_transform.py
@@ -0,0 +1,40 @@
+import librosa
+import librosa.display
+from pathlib import Path
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+#from PyQt5 import QtCore, QtGui, QtWidgets
+#os.environ["QT_API"] = "pyqt5" 
+
+def precompute_spectrograms(dpi=50):
+    dpi = 50
+
+    PATH_TO_WAVs = Path.cwd() / "www" / "wav_transform" / "untransformed_wavs"
+    PATH_TO_PNGs = Path.cwd() / "www" / "current_files" / "audio"
+    #print(PATH_TO_WAVs)
+    files = Path(PATH_TO_WAVs).glob('*.wav')
+    for filename in files:
+        print(filename)
+        audio_tensor, sr = librosa.load(filename, sr=None)
+        spectrogram = librosa.feature.melspectrogram(audio_tensor, sr=sr)
+        log_spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+        librosa.display.specshow(log_spectrogram, sr=sr, x_axis='time', y_axis='mel')
+        plt.gcf().savefig(os.path.join(PATH_TO_PNGs,filename.name) + ".png", dpi=dpi)
+#
+#     #"surprise" wird von 8 auf 0 geändert
+    counter = 0
+    path = Path.cwd() / "www" / "current_files" / "audio"
+    for file in os.listdir(path):
+        #print(file)
+        if file.endswith("png"):
+            if file.find("transformed_png") > -1:
+                counter = counter + 1
+                os.rename(os.path.join(path, file), os.path.join(path, file.replace("-08-", "-00-")))
+    if counter == 0:
+        print("No file has been found")
+# # 
+# # #precompute_spectrograms(PATH_TO_WAVs)
+
