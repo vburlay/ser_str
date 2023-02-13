5aa60239172298dd87d9329c734eb56e0b609887
gordon_soic@mail.ru <gordon_soic@mail.ru>
2023-02-13 20:27:13 +0100

    files

--- workflow/streamlit_app.py
+++ workflow/streamlit_app.py
@@ -3,19 +3,11 @@ Created on Sat Feb 03 23:41:46 2023
 
 @author: Vladimir
 """
-from os.path import dirname, abspath, join
-import sys
 import streamlit as st
 import pandas as pd
-import numpy as np
-
-# Find code directory relative to our directory
-THIS_DIR = dirname(__file__)
-CODE_DIR = abspath(join(THIS_DIR, '../../pythonProject/ser_streamlit', 'code'))
-sys.path.append(CODE_DIR)
 import skript_audio_class
-# Give our app a title
-st.title("SER")
+
 data = pd.DataFrame(data=skript_audio_class.check_wav("www/audio"))
+st.title("SER")
 st.subheader("Raw data")
 st.write(data)
