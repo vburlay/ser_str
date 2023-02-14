f4710f7e2fad778825777c0ec481693d11e89d18
gordon_soic@mail.ru <gordon_soic@mail.ru>
2023-02-14 23:55:28 +0100

    files

--- workflow/streamlit_app.py
+++ workflow/streamlit_app.py
@@ -7,10 +7,14 @@ import streamlit as st
 import pandas as pd
 import skript_audio_class
 import plotly.express as px
+from streamlit_imagegrid import streamlit_imagegrid
+import requests
+from PIL import Image
+from io import BytesIO
 
-class_names = ['neutral', 'happy', 'sad', 'angry', 'fear', 'surprise']
+
 st.sidebar.title("Control Panel")
-#app_mode = st.sidebar.selectbox("App-Mode", ["Application start","Show the source code"])
+
 with st.sidebar:
     add_selectbox = st.selectbox("App-Mode", ["Application start","Show the source code"])
     add_radio = st.radio(
@@ -21,9 +25,78 @@ with st.sidebar:
 if add_selectbox  == "Application start" and add_radio == "Pytorch (ResNet34)":
     data = pd.DataFrame(data=skript_audio_class.check_wav("www/audio"))
     st.title("Speach emotion recognition")
-    if st.checkbox("Show raw data"):
-        st.subheader("Raw data")
-       # st.write(data)
+    if  st.checkbox("Show raw data"):
+
+        zoom_val = st.sidebar.slider('Zoom', 198, 240)
+
+        urls = [
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-09.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-10.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-11.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-12.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-13.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-14.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-15.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-16.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-17.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-18.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-19.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-20.wav.png"
+            }
+        ]
+
+        return_value = streamlit_imagegrid(urls=urls, zoom=zoom_val, height=1000)
+
+        if return_value is not None:
+            response = requests.get(return_value)
+            st.sidebar.markdown('<img src={} width=240px></img>'.format(return_value), unsafe_allow_html=True)
     else:
         fig = px.bar(data)
 
