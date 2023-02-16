52746e1e6da06d81ecb792bc6a7eea5b838d0499
gordon_soic@mail.ru <gordon_soic@mail.ru>
2023-02-16 19:33:21 +0100

    files

--- workflow/streamlit_test.py
+++ workflow/streamlit_test.py
@@ -9,84 +9,96 @@ def get_file_content_as_string(path):
 
 st.sidebar.title("Control Panel")
 
+st.sidebar.title("Control Panel")
+
 with st.sidebar:
     add_selectbox = st.selectbox("App-Mode", ["Application start","Show the source code"])
     add_radio = st.radio(
         "Choose a model",
         ("Pytorch (ResNet34)", "Keras (ResNet34)")   )
 
-    if add_selectbox == "Application start" and add_radio == "Pytorch (ResNet34)":
 
-        st.title("Speach emotion recognition")
-        if not st.checkbox("Show raw data"):
+if add_selectbox  == "Application start" and add_radio == "Pytorch (ResNet34)":
 
-            zoom_val = st.sidebar.slider('Zoom', 183, 240)
+    st.title("Speach emotion recognition")
+    if  st.checkbox("Show raw data"):
 
-            urls = [
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-09.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-10.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-11.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-12.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-13.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-14.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-15.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-16.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-17.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-18.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-19.wav.png"
-                },
-                {
-                    "width": 2000,
-                    "height": 2000,
-                    "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-20.wav.png"
-                }
-            ]
+        zoom_val = st.sidebar.slider('Zoom', 183, 240)
 
-            return_value = streamlit_imagegrid(urls=urls, zoom=zoom_val, height=1000)
+        urls = [
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-09.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-10.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-11.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-12.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-13.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-14.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-15.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-16.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-17.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-18.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-19.wav.png"
+            },
+            {
+                "width": 2000,
+                "height": 2000,
+                "src": "https://raw.githubusercontent.com/vburlay/ser_str/master/date/test/test50_03-01-01-01-02-02-20.wav.png"
+            }
+        ]
 
-            if return_value is not None:
-                response = requests.get(return_value)
-                st.sidebar.markdown('<img src={} width=240px></img>'.format(return_value), unsafe_allow_html=True)
+        return_value = streamlit_imagegrid(urls=urls, zoom=zoom_val, height=1000)
+
+        if return_value is not None:
+            response = requests.get(return_value)
+            st.sidebar.markdown('<img src={} width=240px></img>'.format(return_value), unsafe_allow_html=True)
+    else:
+        tab1, tab2, tab3 = st.tabs(["Countplot of the results", "Result Tabular","Individual results"])
+
+
+
+elif add_selectbox == "Show the source code" and  add_radio == "Pytorch (ResNet34)":
+    readme_text = st.markdown(get_file_content_as_string("streamlit.md"))
+else:
+    st.title("Keras (ResNet34) is not jet included")
